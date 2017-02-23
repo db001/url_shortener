fc8c83a60bd0b7e5e4638bf680fc1c70b2d684af
db001 <dnbennett@hotmail.co.uk>
2017-02-23 21:32:04 +0000

    Insert new url working

--- server.js
+++ server.js
@@ -14,20 +14,34 @@ mongo.connect(url, function(err, db) {
     } else {
         console.log("Database connected at", url);
     }
-    db.close();
+    
+    var coll = db.collection('sites');
 
     app.use('/', express.static(__dirname + '/public'));
 
     app.get('/new/:url', function(req, res) {
-        var reqUrl = req.params.url;
-        console.log("Url");
-        /*
+
         var obj = {
             "original_url": req.params.url,
             "short_url": "https://THIS-URL/1234"
         };
-        */
-        res.send({"test":"testing"});
+
+        coll.insert({
+            obj
+            }, {
+                original_url: 1,
+                short_url: 1,
+                _id: 0        
+            }, function(err, docs) {
+            if (err) {
+                throw new Error('Cannot insert into collection');
+            } else {
+                console.log(docs, "inserted");
+                res.send(obj);
+            }
+        });
+
+        db.close();
     });
 
     var port = process.env.PORT || 3000;
