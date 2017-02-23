1e8799a5451c626f761cb4a3660887cd74d7b69c
db001 <dnbennett@hotmail.co.uk>
2017-02-23 22:53:59 +0000

    validation function for URL added

--- app/logic.js
+++ app/logic.js
@@ -6,20 +6,25 @@ module.exports = function(app, db) {
 
     app.get('/new/:url', function(req, res) {
 
-        var obj = {
-            "original_url": req.params.url,
-            "short_url": generateURL()
-        };
+        if (!validateURL(req.params.url)) {
+            res.send({"original_url": "invalid URL used, most follow 'http://www.example.com' format"});
+        } else {
 
-        res.send(obj);
+            var obj = {
+                "original_url": req.params.url,
+                "short_url": generateURL()
+            };
 
-        coll.insert(obj, function(err, docs) {
-            if (err) {
-                throw new Error('Cannot insert into collection');
-            } else {
-                console.log(docs, "inserted");          
-            }
-        });
+            res.send(obj);
+
+            coll.insert(obj, function(err, docs) {
+                if (err) {
+                    throw new Error('Cannot insert into collection');
+                } else {
+                    console.log(docs, "inserted");          
+                }
+            });
+        }
 
     });
 
@@ -27,6 +32,18 @@ module.exports = function(app, db) {
     function generateURL() {
         var randomNum = ("000" + Math.floor(Math.random() * 1000)).slice(-4);
         return "https://THIS-URL/" + randomNum;
+    }
+
+    function validateURL(url) {
+        // regex to match valid URL: (http|https):\/\/www\.\w+.\w+(\.\w+)?
+        var patt = /(http|https):\/\/www\.\w+.\w+(\.\w+)?/g;
+
+        if (patt.test(url)) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
 }
