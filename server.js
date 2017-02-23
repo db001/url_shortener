8e0feaef4281aff5bdbb8a389a673983dd42f243
db001 <dnbennett@hotmail.co.uk>
2017-02-23 21:04:07 +0000

    new url params added - not working!

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb').MongoClient;
 
-// Mongodb db url
+// Mongo db url
 var url = 'mongodb://localhost:27017/urls';
 
 var app = express();
@@ -18,6 +18,17 @@ mongo.connect(url, function(err, db) {
 
     app.use('/', express.static(__dirname + '/public'));
 
+    app.get('/new/:url', function(req, res) {
+        var reqUrl = req.params.url;
+        console.log("Url");
+        /*
+        var obj = {
+            "original_url": req.params.url,
+            "short_url": "https://THIS-URL/1234"
+        };
+        */
+        res.send({"test":"testing"});
+    });
 
     var port = process.env.PORT || 3000;
 
