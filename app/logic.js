d10567952888bba1c691fcddb17c5acb04f9a0d0
db001 <dnbennett@hotmail.co.uk>
2017-02-24 14:20:26 +0000

    No functional changes, code tweaking only

--- app/logic.js
+++ app/logic.js
@@ -6,16 +6,20 @@ module.exports = function(app, db) {
 
     app.get('/new/:url', function(req, res) {
 
-        if (!validateURL(req.params.url)) {
-            res.send({"original_url": "invalid URL used, most follow 'http://www.example.com' format"});
-        } else {
+        var obj = {};
 
-            var obj = {
-                "original_url": req.params.url,
-                "short_url": generateURL()
+        if (!validateURL(req.params.url)) {
+
+            obj = {"original_url": "invalid URL used, please use 'http://www.example.com' format"};
+
+        } else {
+            
+            obj = {
+            "original_url": req.params.url,
+            "short_url": generateURL()
             };
 
-            res.send(obj);
+            
 
             coll.insert(obj, function(err, docs) {
                 if (err) {
@@ -26,6 +30,8 @@ module.exports = function(app, db) {
             });
         }
 
+        res.send(obj);
+        
     });
 
     // generate random URL number
@@ -34,16 +40,18 @@ module.exports = function(app, db) {
         return "https://THIS-URL/" + randomNum;
     }
 
+    // check validity of url
     function validateURL(url) {
-        // regex to match valid URL: (http|https):\/\/www\.\w+.\w+(\.\w+)?
-        var patt = /(http|https):\/\/www\.\w+.\w+(\.\w+)?/g;
+
+        var patt = /(http|https):\/\/www\.\w+\.\w+(\.\w+)?/g;
 
         if (patt.test(url)) {
+            console.log("Valid URL");
             return true;
         } else {
+            console.log("Invalid URL");
             return false;
         }
-
     }
 
 }
